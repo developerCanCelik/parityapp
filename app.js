8be738cca3860ca0131370d88b768645c3646c83
Can Çelik <73348407+developerCanCelik@users.noreply.github.com>
2021-03-24 19:44:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Elementleri seçme
+// Selected Element
 const amountElement = document.querySelector("#amount")
 const firstSelect = document.querySelector("#firstCurrency")
 const secondSelect = document.querySelector("#secondCurrency")
@@ -8,7 +8,6 @@ eventListener()
 function eventListener(){
     amountElement.addEventListener("input",exchangeCurrency);
     firstSelect.onchange = function(){
-        //hangi options seçildiğiyse onu alma işlemi yaptık
         currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent)
         ui.changeFirst()
     }
